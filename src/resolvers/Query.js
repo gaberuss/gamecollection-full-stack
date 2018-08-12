52e5db70e5e5f43bcba9e3bf9e29cd2d3439e14b
Gabriel <gabrielruss88@gmail.com>
2018-08-12 16:40:37 -0400

    added inventory

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,5 +1,10 @@
 const info = () => 'This is my game collection API'
 
+function inventory(root, args, context, info) {
+  return context.db.query.games({}, info)
+}
+
 module.exports = {
   info,
+  inventory,
 }
