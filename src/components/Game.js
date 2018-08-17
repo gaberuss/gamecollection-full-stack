b08838e18396d420db8d11666ccb22dda8b5dc74
Gabriel <gabrielruss88@gmail.com>
2018-08-16 21:21:20 -0400

    changing css

--- src/components/Game.js
+++ src/components/Game.js
@@ -6,10 +6,10 @@ class Game extends Component {
       <main className="mw6 left">
         <article className="dt w-100 bb b--black-05 pb2 mt2" href="#0">
           <div className="dtc v-mid pl3">
-            <h1 className="f6 f5-ns fw6 lh-title black mv0">
+            <h1 className="f6 f5-ns fw6 lh-title black mv0 dark-gray">
               {this.props.game.name}
             </h1>
-            <h2 className="f6 fw4 mt0 mb0 black-60">
+            <h2 className="f6 fw6 mt0 mb0 black-60">
               {this.props.game.gameConsole}
             </h2>
             <h3 className="f6 fw4 mt0 mb0 black-60">
