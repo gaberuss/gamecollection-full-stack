20b68c80fb993491c4cd65fcba78a502b2b69807
Gabriel <gabrielruss88@gmail.com>
2018-08-19 19:49:03 -0400

    added create game form

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
 import GameList from './GameList'
+import CreateGame from './CreateGame'
 
 class App extends Component {
   render() {
-    return <GameList />
+    return <CreateGame />
   }
 }
 
---
+++ src/components/CreateGame.js
@@ -0,0 +1,82 @@
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+
+const CREATE_GAME_MUTATION = gql`
+  mutation CreateGameMutation(
+    $name: String!
+    $gameConsole: String!
+    $condition: String!
+  ) {
+    createGame(name: $name, gameConsole: $gameConsole, condition: $condition) {
+      id
+      createdAt
+      name
+      gameConsole
+      condition
+    }
+  }
+`
+
+const inputStyling = 'input-reset ba b--black-20 pa2 mb2 db w-100'
+
+class CreateGame extends Component {
+  state = {
+    name: '',
+    gameConsole: '',
+    condition: '',
+  }
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  render() {
+    const { name, gameConsole, condition } = this.state
+    return (
+      <div>
+        <div className="flex flex-column mt3">
+          <input
+            className={inputStyling}
+            name="name"
+            value={name}
+            onChange={this.handleChange}
+            type="text"
+            placeholder="A name for the game"
+          />
+          <input
+            className={inputStyling}
+            name="gameConsole"
+            value={gameConsole}
+            onChange={this.handleChange}
+            type="text"
+            placeholder="The console for this game"
+          />
+          <input
+            className={inputStyling}
+            name="condition"
+            value={condition}
+            onChange={this.handleChange}
+            type="text"
+            placeholder="What is the condition of the game?"
+          />
+        </div>
+        <Mutation
+          mutation={CREATE_GAME_MUTATION}
+          variables={{ name, gameConsole, condition }}
+        >
+          {createGameMutation => (
+            <button
+              className="f6 link dim br2 ba ph3 pv2 mb2 dib dark-gray"
+              onClick={createGameMutation}
+            >
+              Add Game
+            </button>
+          )}
+        </Mutation>
+      </div>
+    )
+  }
+}
+
+export default CreateGame
