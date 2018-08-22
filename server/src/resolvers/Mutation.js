15a48547bed321d7cd1ebbd2e26cfb7e3105847a
Gabriel <gabrielruss88@gmail.com>
2018-08-22 19:52:47 -0400

    added update and delete

--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -11,6 +11,29 @@ function createGame(parent, args, context, info) {
   )
 }
 
+function updateGame(parent, args, context, info) {
+  return context.db.mutation.updateGame({
+    where: {
+      id: args.id,
+    },
+    data: {
+      name: args.name,
+      gameConsole: args.gameConsole,
+      condition: args.condition,
+    },
+  })
+}
+
+function deleteGame(parent, args, context, info) {
+  return context.db.mutation.deleteGame({
+    where: {
+      id: args.id,
+    },
+  })
+}
+
 module.exports = {
   createGame,
+  updateGame,
+  deleteGame,
 }
