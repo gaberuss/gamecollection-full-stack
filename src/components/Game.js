31eecbf54d40669c57a2e0a9e37ece1b89c4a0bc
Gabriel <gabrielruss88@gmail.com>
2018-08-27 19:57:18 -0400

    added edit button html and css

--- src/components/Game.js
+++ src/components/Game.js
@@ -23,28 +23,22 @@ const DELETE_GAME_MUTATION = gql`
 
 class Game extends Component {
   render() {
-    const { id } = this.props.game
-    console.log({ id })
+    const { id, name, gameConsole, condition } = this.props.game
     return (
       <main className="mw6 left">
         <article className="dt w-100 bb b--black-05 pb2 mt2" href="#0">
           <div className="dtc v-mid pl3">
             <h1 className="f6 f5-ns fw6 lh-title black mv0 dark-gray">
-              {this.props.game.name}
+              {name}
             </h1>
-            <h2 className="f6 fw6 mt0 mb0 black-60">
-              {this.props.game.gameConsole}
-            </h2>
-            <h3 className="f6 fw4 mt0 mb0 black-60">
-              Condition: {this.props.game.condition}
-            </h3>
+            <h2 className="f6 fw6 mt0 mb0 black-60">{gameConsole}</h2>
+            <h3 className="f6 fw4 mt0 mb0 black-60">Condition: {condition}</h3>
           </div>
           <Mutation
             mutation={DELETE_GAME_MUTATION}
             variables={{ id }}
             update={(cache, { data: { deleteGame } }) => {
               const { inventory } = cache.readQuery({ query: INVENTORY_QUERY })
-              console.log(inventory)
               cache.writeQuery({
                 query: INVENTORY_QUERY,
                 data: { inventory: inventory.filter(game => game.id !== id) },
@@ -57,6 +51,12 @@ class Game extends Component {
               </button>
             )}
           </Mutation>
+          <button
+            className="edit-button"
+            onClick={() => alert(`Editing item ${id}`)}
+          >
+            Edit
+          </button>
         </article>
       </main>
     )
